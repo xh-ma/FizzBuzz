5d7535c07c55084832821f07786ab8de3cb777d5
xh-ma <xiaohan.m9@gmail.com>
2025-09-09 12:50:45 -0400

    Task 4

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples(1000,3,5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int multiples(){
+        return Multiples.multiples(1000,3,5);
+    }
+}
